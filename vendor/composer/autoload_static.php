3a0bf9aea5d92f1318be573ed751500da2cbd863
Théo Delaporte <theodelaporte@MacBook-de-Theo.local>
2022-05-02 09:20:41 +0200

    config + composer bootstrap + homepage

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -152,8 +152,8 @@ class ComposerStaticInit00d277ea7d792c11ae400f7d763371fb
         'phpDocumentor\\Reflection\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
         ),
         'Webmozart\\Assert\\' => 
         array (
@@ -254,8 +254,8 @@ class ComposerStaticInit00d277ea7d792c11ae400f7d763371fb
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
